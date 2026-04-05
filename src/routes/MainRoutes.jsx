b0325a2a1b91d585a8df9b09e1c47a130509948e
Abhijeet kumar <abhijeetkumar@Abhijeets-MacBook-Air.local>
2026-04-05 19:33:54 +0530

    added routing

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -1,14 +1,16 @@
-import { lazy } from "react";
+import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 const Dashboard = lazy(() => import("../pages/Dashboard"));
 const Transaction = lazy(() => import("../pages/Transaction"));
 
 const MainRoutes = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Dashboard />} />
-      <Route path="/transaction" element={<Transaction />} />
-    </Routes>
+    <Suspense fallback={null}>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/transaction" element={<Transaction />} />
+      </Routes>
+    </Suspense>
   );
 };
 
